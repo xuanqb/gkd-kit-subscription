33b97e1f01ba1950fefe4b95b894bfb03ab384a1
xuanqb <xuanqb@qq.com>
2023-09-07 15:43:32 +0800

    feat(boss直聘): 忽略活跃度低的boss

--- src/apps/com.hpbr.bosszhipin.ts
+++ src/apps/com.hpbr.bosszhipin.ts
@@ -18,7 +18,7 @@ export default defineAppConfig(
           '[id="com.hpbr.bosszhipin:id/tv_improper"]',
           '[text="BOSS活跃度低"]',
           '[text="面试过/入职过"]',
-          '[id="com.hpbr.bosszhipin:id/boss_label_tv"][text="本月活跃"||text="近半年活跃"] <n ViewGroup <n RecyclerView < FrameLayout < ViewPager < RelativeLayout > FrameLayout[id="com.hpbr.bosszhipin:id/title_action_view"] >n [id="com.hpbr.bosszhipin:id/iv_another"]'
+          '[id="com.hpbr.bosszhipin:id/boss_label_tv"][text="本周活跃"||text="本月活跃"||text="近半年活跃"||text$="周内活跃"||text$="月内活跃"] <n ViewGroup <n RecyclerView < FrameLayout <n ViewPager < RelativeLayout > FrameLayout[id="com.hpbr.bosszhipin:id/title_action_view"] >n [id="com.hpbr.bosszhipin:id/iv_another"]'
         ],
       },
     ],
