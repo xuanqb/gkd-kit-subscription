a816fd6a9b48588f450d449ef1490b929e300441
xuanqb <xuanqb@qq.com>
2023-09-07 08:57:52 +0800

    feat(百度网盘): 首屏跳过

--- src/apps/com.baidu.netdisk.ts
+++ src/apps/com.baidu.netdisk.ts
@@ -7,7 +7,7 @@ export default defineAppConfig({
     {
       key: 0,
       name: '开屏广告',
-      activityIds: 'com.baidu.netdisk.ui.Navigate',
+      activityIds: ['com.baidu.netdisk.ui.Navigate','com.baidu.netdisk.ui.MainActivity'],
       rules: [
         'TextView[text^="跳过"][clickable=true]',
         "[id='com.byted.pangle:id/tt_splash_skip_btn']", // 1687136781353
