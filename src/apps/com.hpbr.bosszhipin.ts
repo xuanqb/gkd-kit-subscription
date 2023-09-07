45b4f299c39d047a6434eb73a87194a171fd0669
xuanqb <xuanqb@qq.com>
2023-09-07 09:52:04 +0800

    feat(boss直聘): 不感兴趣

--- src/apps/com.hpbr.bosszhipin.ts
+++ src/apps/com.hpbr.bosszhipin.ts
@@ -1,27 +1,31 @@
 import {defineAppConfig} from '../types';
 
 export default defineAppConfig(
-    {
-        id: 'com.hpbr.bosszhipin',
-        name: 'boss直聘',
-        groups: [
-            {
-                key: 1,
-                name: '跳过不感兴趣',
-                activityIds: ['com.hpbr.bosszhipin.module.position.BossJobPagerActivity',
-                    'com.monch.lbase.dialog.ProgressDialog', 'com.hpbr.bosszhipin.module.main.activity.MainActivity'],
-                rules: [
-                    {
-                        matches: '[id="com.hpbr.bosszhipin:id/tv_improper"]'
-                    },
-                    {
-                        matches: '[text="BOSS活跃度低"]'
-                    },
-                    {
-                        matches: '[text="面试过/入职过"]'
-                    }
-                ],
-            },
+  {
+    id: 'com.hpbr.bosszhipin',
+    name: 'boss直聘',
+    groups: [
+      {
+        key: 1,
+        name: '跳过不感兴趣',
+        activityIds: [
+          'com.hpbr.bosszhipin.module.position.BossJobPagerActivity',
+          'com.hpbr.bosszhipin.module.map.activity.GeekRoute1004NewActivity',
+          'com.monch.lbase.dialog.ProgressDialog',
+          'com.hpbr.bosszhipin.module.main.activity.MainActivity'
         ],
-    }
+        rules: [
+          {
+            matches: '[id="com.hpbr.bosszhipin:id/tv_improper"]'
+          },
+          {
+            matches: '[text="BOSS活跃度低"]'
+          },
+          {
+            matches: '[text="面试过/入职过"]'
+          }
+        ],
+      },
+    ],
+  }
 );
